af997ee12124c7cd613672970f0a3ae554a2096f
Zakhar Gadzhiev <soulghai0@gmail.com>
2017-08-07 18:28:12 +0300

    Удалил мусор

---
+++ Assets/Scripts/ScreenGift.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScreenGift : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void OnEnable()
+	{
+		GlobalEvents<OnBtnGiftClick>.Happened += OnBtnGiftClick;
+	}
+
+	private void OnDisable()
+	{
+		GlobalEvents<OnBtnGiftClick>.Happened -= OnBtnGiftClick;
+	}
+
+	private void OnBtnGiftClick(OnBtnGiftClick obj)
+	{
+		
+	}
+}
