6fe9329304fe405441759114cf59e9fb693fb1a9
Zakhar Gadzhiev <soulghai0@gmail.com>
2017-10-04 18:19:30 +0400

    Добавил новый класс для отслеживания управления игроком

---
+++ Assets/Scripts/UI/GameInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInput : MonoBehaviour
+{
+	private Image _image;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_image = GetComponent<Image>();
+		GlobalEvents<OnGameInputEnable>.Happened += OnGameInputEnable;
+	}
+
+	private void OnGameInputEnable(OnGameInputEnable obj)
+	{
+		_image.raycastTarget = obj.Flag;
+	}
+}
