1ebddd3c822370493ad9ab3715564798e5521490
Zakhar Gadzhiev <soulghai0@gmail.com>
2017-11-14 15:03:07 +0400

    SecureInt Fixed 2

---
+++ Assets/Scripts/Framework/Types/SecureInt.cs
@@ -0,0 +1,33 @@
+﻿using Random = UnityEngine.Random;
+
+public struct SecureInt
+{
+    private int _value;
+    private int _offset;
+
+    public SecureInt(int value)
+    {
+        _offset = Random.Range(-1000, 1000);
+        _value = value + _offset;
+    }
+
+    public int GetValue()
+    {
+        return _value - _offset;
+    }
+
+    public override string ToString()
+    {
+        return GetValue().ToString();
+    }
+
+    public static SecureInt operator +(SecureInt i1, SecureInt i2)
+    {
+        return new SecureInt(i1.GetValue() + i2.GetValue());
+    }
+    
+    public static SecureInt operator -(SecureInt i1, SecureInt i2)
+    {
+        return new SecureInt(i1.GetValue() - i2.GetValue());
+    }
+}
